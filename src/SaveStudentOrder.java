2892c87597a7a16bbde3e98256ba3c70fa5110b8
Dmitriy Pushkarev <dum3712@mail.ru>
2023-08-03 12:56:11 +0400

    Update SaveStudentOrder, add StudentOrderValidator

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,2 +1,12 @@
+import java.sql.SQLOutput;
+
 public class SaveStudentOrder {
+    public static void main(String[] args) {
+        saveStudentOrder();
+    }
+    static void saveStudentOrder(){
+        System.out.println("saveStudentOrder 1");
+
+    }
+
 }
