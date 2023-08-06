b051ece625d2590bdcc708ccb0af440bfa83176c
Dmitriy Pushkarev <dum3712@mail.ru>
2023-08-06 16:56:03 +0400

    Add answer functional

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -2,11 +2,22 @@ import java.sql.SQLOutput;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        saveStudentOrder();
-    }
-    static void saveStudentOrder(){
-        System.out.println("saveStudentOrder 1");
+        StudentOrder so;
+        so = new StudentOrder();
+        so.hFirstName="Alex";
+        so.hLastName="Petrov";
+        so.wFirstName="Galina";
+        so.wLastName="Petrova";
 
+        long ans=saveStudentOrder(so);
+        System.out.println(ans);
+    }
+    static long saveStudentOrder(StudentOrder studentOrder){
+        long answer;
+        answer =199;
+        System.out.println("saveStudentOrder:"+studentOrder.hLastName);
+
+        return answer;
     }
 
 }
