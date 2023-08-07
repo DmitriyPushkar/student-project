5085e173c14a52483aebe72826f4f3ceb4209a2d
Dmitriy Pushkarev <dum3712@mail.ru>
2023-08-07 21:42:29 +0400

    Add inherited methods

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,16 +1,14 @@
 package edu.javacourse.studentorder;
 
-import edu.javacourse.studentorder.domain.Adult;
+import edu.javacourse.studentorder.domain.other.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
-
-import java.sql.SQLOutput;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        StudentOrder so=new StudentOrder();
-
-        long ans=saveStudentOrder(so);
-        System.out.println(ans);
+        buildStudentOrder();
+        //      StudentOrder so=new StudentOrder();
+        // ans=saveStudentOrder(so);
+        //       System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer;
@@ -23,7 +21,12 @@ public class SaveStudentOrder {
         StudentOrder so=new StudentOrder();
         Adult husband=new Adult();
         husband.setGivenName("Евгений");
+        husband.setSurName("Petrov");
+        husband.setPassportNumber("123456");
         so.setHusband(husband);
+        String ans = husband.getPersonString();
+        System.out.println(ans);
+
         return so;
     }
 
