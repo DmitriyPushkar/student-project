d8e115169b559e5367b6c047a7532334ffc065b5
Dmitriy Pushkarev <dum3712@mail.ru>
2023-08-07 18:40:07 +0400

    Add getters and setters

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -7,22 +7,17 @@ import java.sql.SQLOutput;
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so=new StudentOrder();
-        so.hFirstName="Alex";
-        so.hLastName="Petrov";
-        so.wFirstName="Galina";
-        so.wLastName="Petrova";
-        StudentOrder so1=new StudentOrder();
-        so1.hFirstName="Alex";
-        so1.hLastName="Petrov";
-        so1.wFirstName="Galina";
-        so1.wLastName="Petrova";
+        so.sethFirstName("Alex");
+        so.sethLastName("Petrov");
+        so.setwFirstName("Galina");
+        so.setwLastName("Petrova");
         long ans=saveStudentOrder(so);
         System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer;
         answer =199;
-        System.out.println("saveStudentOrder:"+studentOrder.hLastName);
+        System.out.println("saveStudentOrder:"+studentOrder.gethLastName());
 
         return answer;
     }
